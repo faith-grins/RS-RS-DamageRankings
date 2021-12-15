f8a59b0f737876d969adacc7c5816103cf755e13
faith-grins <quietkane@gmail.com>
2021-12-14 19:21:26 -0600

    Fixed some issues with damage formula. (Yay tests)

--- data/model/Character.py
+++ data/model/Character.py
@@ -16,7 +16,8 @@ class Character:
         self.max_base_cha_value = 0
         self.base_stat_cap = 0
 
-    def update_base_stats(self):
+    def update_base_stats(self, stat_cap):
+        self.base_stat_cap = stat_cap
         for style in self.styles:
             if style.str_bonus > self.max_base_str_value:
                 self.max_base_str_value = style.str_bonus
@@ -35,19 +36,24 @@ class Character:
             if style.cha_bonus > self.max_base_cha_value:
                 self.max_base_cha_value = style.cha_bonus
 
+    def add_styles(self, styles_list):
+        for style in styles_list:
+            if style.character_name == self.name:
+                self.styles.append(style)
+
     def attack(self, style, skill, weapon, formation_boost, equipment_mods, mastery_level,
                enemy_end, enemy_wil, enemy_resist, turn_number, full_hp):
         rank = 99
         skill_power = skill.power_number
-        rank_modifier = (skill_power - 5) * ((100 + rank) / 100)
-        final_str = int((self.base_stat_cap + self.max_base_str_value) * style.level_50_str_mod *
-                        (100 + formation_boost) / 100 + style.str_bonus + equipment_mods.str)
-        final_agi = int((self.base_stat_cap + self.max_base_agi_value) * style.level_50_agi_mod *
-                        (100 + formation_boost) / 100 + style.agi_bonus + equipment_mods.agi)
-        final_dex = int((self.base_stat_cap + self.max_base_dex_value) * style.level_50_dex_mod *
-                        (100 + formation_boost) / 100 + style.dex_bonus + equipment_mods.dex)
-        final_int = int((self.base_stat_cap + self.max_base_int_value) * style.level_50_int_mod *
-                        (100 + formation_boost) / 100 + style.int_bonus + equipment_mods.int)
+        rank_modifier = (skill_power - 5) * (100 + rank) / 100
+        final_str = int((self.base_stat_cap + self.max_base_str_value) * (100 + style.level_50_str_mod +
+                        formation_boost) / 100 + style.str_bonus + equipment_mods.str)
+        final_agi = int((self.base_stat_cap + self.max_base_agi_value) * (100 + style.level_50_agi_mod +
+                        formation_boost) / 100 + style.agi_bonus + equipment_mods.agi)
+        final_dex = int((self.base_stat_cap + self.max_base_dex_value) * (100 + style.level_50_dex_mod +
+                        formation_boost) / 100 + style.dex_bonus + equipment_mods.dex)
+        final_int = int((self.base_stat_cap + self.max_base_int_value) * (100 + style.level_50_int_mod +
+                        formation_boost) / 100 + style.int_bonus + equipment_mods.int)
         if weapon.type == WeaponType.Fist:
             stat_factor = 1 + 2 * final_str + 2.5 * final_agi - 1.2 * enemy_end
             weapon_factor = weapon.max_wp
@@ -67,13 +73,26 @@ class Character:
         resist_factor = 1 / (1 + 0.008 * enemy_resist)
         weak_point = enemy_resist <= -35
         ability_factor = sum([a.damage_increase(skill.damage_types, turn_number, full_hp, weak_point)
-                              for a in style.abilities]) / 100
-        mastery_factor = round(mastery_level / 2) * 0.5
-        random_factors = range(1, 7)
+                              for a in style.abilities])
+        mastery_factor = round(mastery_level / 2) * 0.5 - 0.5
+        random_factors = range(1, 11)
         damage_values = []
         for r in random_factors:
-            random_factor = ability_factor + mastery_factor + (r - 6) / 100
-            damage = (weapon_factor + skill_power * rank_modifier) * stat_factor * resist_factor * random_factor
+            random_factor = 1 + (ability_factor + mastery_factor + r - 6) / 100
+            technique_factor = weapon_factor + skill_power + rank_modifier
+            damage = technique_factor * stat_factor * resist_factor * random_factor / 10
             damage_values.append(int(damage))
-        return int(sum(damage_values) / len (damage_values))
+        # return int(sum(damage_values) / len(damage_values))
+        return damage_values
 
+
+def get_characters(character_filename, styles_list):
+    from json import load
+    with open(character_filename, 'r') as character_file:
+        character_json_list = load(character_file)
+    character_list = []
+    for json_character in character_json_list:
+        character = Character(json_character)
+        character.add_styles(styles_list)
+        character_list.append(character)
+    return character_list
--- tests/damage_tests.py
+++ tests/damage_tests.py
@@ -1,4 +1,4 @@
-from data.model import Character, Equipment
+from data.model import Character, Equipment, Common
 from data import load_styles
 
 
@@ -15,14 +15,15 @@ def test_vortex_breaker():
     valdor = Character.Character(json_character)
     styles = load_styles()
     valdor.add_styles(styles)
-    valdor.update_base_stats(163)
+    valdor.update_base_stats(160)
     selected_style = [style for style in valdor.styles if style.style_name == '[Fruits of My Training]'][0]
     selected_skill = [skill for skill in selected_style.skills if skill.name == 'Vortex Breaker'][0]
     equips = Equipment.EquipmentBonus()
     equips.str = 17
-    equips.agi = 24
+    equips.agi = 32
     weapon = Equipment.Weapon("Cat's Claws")
-    weapon.max_wp = 35
+    weapon.max_wp = 45
+    weapon.type = Common.WeaponType.Fist
     damage_values = valdor.attack(selected_style, selected_skill, weapon, 0, equips, 35, 85, 85, -35, 1, True)
     print(damage_values)
 
