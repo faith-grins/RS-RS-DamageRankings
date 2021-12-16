1d712369366789b3eb0a378c8134808fcd5f870f
faith-grins <quietkane@gmail.com>
2021-12-16 14:53:00 -0600

    Handle base stat adjustments by style correctly.  Add weapon stone to damage formula.  Add support for multi-stat formation positions.

--- data/model/Character.py
+++ data/model/Character.py
@@ -19,41 +19,42 @@ class Character:
     def update_base_stats(self, stat_cap):
         self.base_stat_cap = stat_cap
         for style in self.styles:
-            if style.str_bonus > self.max_base_str_value:
-                self.max_base_str_value = style.str_bonus
-            if style.end_bonus > self.max_base_end_value:
-                self.max_base_end_value = style.end_bonus
-            if style.dex_bonus > self.max_base_dex_value:
-                self.max_base_dex_value = style.dex_bonus
-            if style.agi_bonus > self.max_base_agi_value:
-                self.max_base_agi_value = style.agi_bonus
-            if style.int_bonus > self.max_base_int_value:
-                self.max_base_int_value = style.int_bonus
-            if style.wil_bonus > self.max_base_wil_value:
-                self.max_base_wil_value = style.wil_bonus
-            if style.lov_bonus > self.max_base_lov_value:
-                self.max_base_lov_value = style.lov_bonus
-            if style.cha_bonus > self.max_base_cha_value:
-                self.max_base_cha_value = style.cha_bonus
+            if style.base_str_bonus > self.max_base_str_value:
+                self.max_base_str_value = style.base_str_bonus
+            if style.base_end_bonus > self.max_base_end_value:
+                self.max_base_end_value = style.base_end_bonus
+            if style.base_dex_bonus > self.max_base_dex_value:
+                self.max_base_dex_value = style.base_dex_bonus
+            if style.base_agi_bonus > self.max_base_agi_value:
+                self.max_base_agi_value = style.base_agi_bonus
+            if style.base_int_bonus > self.max_base_int_value:
+                self.max_base_int_value = style.base_int_bonus
+            if style.base_wil_bonus > self.max_base_wil_value:
+                self.max_base_wil_value = style.base_wil_bonus
+            if style.base_lov_bonus > self.max_base_lov_value:
+                self.max_base_lov_value = style.base_lov_bonus
+            if style.base_cha_bonus > self.max_base_cha_value:
+                self.max_base_cha_value = style.base_cha_bonus
 
     def add_styles(self, styles_list):
         for style in styles_list:
             if style.character_name == self.name:
                 self.styles.append(style)
 
-    def attack(self, style, skill, weapon, formation_boost, equipment_mods, mastery_level,
-               enemy_end, enemy_wil, enemy_resist, turn_number, full_hp):
-        rank = 99
+    def attack(self, style, skill, skill_rank, weapon, formation_boost, equipment_mods, mastery_level,
+               enemy_end, enemy_wil, enemy_resist, turn_number, full_hp, weapon_stone):
+        # rank = 99
+        rank = skill_rank
         skill_power = skill.power_number
         rank_modifier = (skill_power - 5) * (100 + rank) / 100
         final_str = int((self.base_stat_cap + self.max_base_str_value) * (100 + style.level_50_str_mod +
-                        formation_boost) / 100 + style.str_bonus + equipment_mods.str)
+                        formation_boost.str) / 100 + style.str_bonus + equipment_mods.str + 8)
         final_agi = int((self.base_stat_cap + self.max_base_agi_value) * (100 + style.level_50_agi_mod +
-                        formation_boost) / 100 + style.agi_bonus + equipment_mods.agi)
+                        formation_boost.agi) / 100 + style.agi_bonus + equipment_mods.agi + 8)
         final_dex = int((self.base_stat_cap + self.max_base_dex_value) * (100 + style.level_50_dex_mod +
-                        formation_boost) / 100 + style.dex_bonus + equipment_mods.dex)
+                        formation_boost.dex) / 100 + style.dex_bonus + equipment_mods.dex + 8)
         final_int = int((self.base_stat_cap + self.max_base_int_value) * (100 + style.level_50_int_mod +
-                        formation_boost) / 100 + style.int_bonus + equipment_mods.int)
+                        formation_boost.int) / 100 + style.int_bonus + equipment_mods.int + 8)
         if skill.weapon_type == WeaponType.Fist:
             stat_factor = 1 + 2 * final_str + 2.5 * final_agi - 1.2 * enemy_end
             weapon_factor = weapon.max_wp
@@ -74,6 +75,7 @@ class Character:
         weak_point = enemy_resist <= -35
         ability_factor = sum([a.damage_increase(skill.damage_types, turn_number, full_hp, weak_point)
                               for a in style.abilities])
+        ability_factor += weapon_stone
         mastery_factor = round((mastery_level - 1) / 2) * 0.5
         random_factors = range(1, 11)
         damage_values = []
