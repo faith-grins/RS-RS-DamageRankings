666e77d786b1cdd2606215ae4ee33840f35f8fb1
faith-grins <quietkane@gmail.com>
2021-12-27 23:11:48 -0600

    Delete stylenames.py

--- stylenames.py
+++
@@ -1,5 +0,0 @@
-with open(r'C:\Users\alanb\Desktop\stylenames.txt', 'r') as input_file:
-    names = [i.strip() for i in input_file.read().split('\n')]
-names.sort(key=str.__len__)
-for i in range(50):
-    print(names[-(i+1)])
