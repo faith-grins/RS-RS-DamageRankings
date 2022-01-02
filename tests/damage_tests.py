2880eb6d1d3539f9762f6c50ceff222d0ec94d7c
faith-grins <quietkane@gmail.com>
2022-01-02 12:57:16 -0600

    Adding some more damage numbers.

--- tests/damage_tests.py
+++ tests/damage_tests.py
@@ -294,7 +294,7 @@ def lunar_blade_vs_urps():
     json_character = {'name': 'Silver', 'id': '00001'}
     silver = Character.Character(json_character)
     silver.add_styles(styles)
-    silver.update_base_stats(124)
+    silver.update_base_stats(125)
     selected_style = [style for style in silver.styles if style.style_name == '[Silver-Style Test of Guts]'][0]
     storm_roar = [skill for skill in selected_style.skills if skill.name == 'Storm Roar'][0]
     silver_attack = silver.attack(selected_style, storm_roar, skill_rank, weapon, formation, equips,
@@ -304,7 +304,7 @@ def lunar_blade_vs_urps():
     json_character = {'name': 'Urpina', 'id': '00002'}
     urpina = Character.Character(json_character)
     urpina.add_styles(styles)
-    urpina.update_base_stats(124)
+    urpina.update_base_stats(125)
     selected_style = [style for style in urpina.styles if style.style_name == "[Now That's Dual Wielding!]"][0]
     cross_break = [skill for skill in selected_style.skills if skill.name == 'Cross Break'][0]
     cross_break.power_number = 28
@@ -314,22 +314,76 @@ def lunar_blade_vs_urps():
     urpina_attack = urpina.attack(selected_style, cross_break, skill_rank, weapon, formation, equips,
                                    mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
                                    weapon_stone)
+    cross_break.power_number = 36
+    dual_whirlwind_attack = urpina.attack(selected_style, cross_break, skill_rank, weapon, formation, equips,
+                                   mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
+                                   weapon_stone)
     # Fancy Lass
     json_character = {'name': 'Final Empress', 'id': '00003'}
     fancy_lass = Character.Character(json_character)
     fancy_lass.add_styles(styles)
-    fancy_lass.update_base_stats(124)
+    fancy_lass.update_base_stats(125)
     selected_style = [style for style in fancy_lass.styles if style.style_name == "[One Special Summer Day]"][0]
     lunar_blade = [skill for style in fancy_lass.styles for skill in style.skills if skill.name == 'Lunar Blade'][0]
     fancy_lass_attack = fancy_lass.attack(selected_style, lunar_blade, skill_rank, weapon, formation, equips,
                                   mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
                                   weapon_stone)
-    silver_damage = sum(silver_attack) / 10
-    urpina_damage = sum(urpina_attack) / 10
-    fancy_lass_damage = sum(fancy_lass_attack) / 10
+    elegant_sands = [skill for skill in selected_style.skills if skill.name == 'Elegant Sands'][0]
+    elegant_sands_attack = fancy_lass.attack(selected_style, elegant_sands, skill_rank, weapon, formation, equips,
+                                          mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
+                                          weapon_stone)
+    # Fancy Lad
+    json_character = {'name': 'Final Emperor', 'id': '00003'}
+    fancy_lad = Character.Character(json_character)
+    fancy_lad.add_styles(styles)
+    fancy_lad.update_base_stats(125)
+    selected_style = [style for style in fancy_lad.styles if style.style_name == "[At the Victory Banquet]"][0]
+    imperial_sword = [skill for style in fancy_lad.styles for skill in style.skills if skill.name == 'Imperial Sword'][0]
+    imperial_sword.power_number = 28
+    imperial_sword_attack = fancy_lad.attack(selected_style, imperial_sword, skill_rank, weapon, formation, equips,
+                                          mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
+                                          weapon_stone)
+    imperial_sword.power_number = 39
+    wheel_swing_attack = fancy_lad.attack(selected_style, imperial_sword, skill_rank, weapon, formation, equips,
+                                          mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
+                                          weapon_stone)
+    # Noel
+    json_character = {'name': 'Noel', 'id': '00003'}
+    noel = Character.Character(json_character)
+    noel.add_styles(styles)
+    noel.update_base_stats(125)
+    selected_style = [style for style in noel.styles if style.style_name == "[Fallen Hero]"][0]
+    steel_wave = [skill for style in noel.styles for skill in style.skills if skill.name == 'Steel Wave'][0]
+    steel_wave_attack = noel.attack(selected_style, steel_wave, skill_rank, weapon, formation, equips,
+                                          mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
+                                          weapon_stone)
+    # Gustave
+    json_character = {'name': 'Gustave', 'id': '00003'}
+    gustave = Character.Character(json_character)
+    gustave.add_styles(styles)
+    gustave.update_base_stats(125)
+    selected_style = [style for style in gustave.styles if style.style_name == "[Leading the Steel Forces]"][0]
+    gustave_attack = gustave.attack(selected_style, steel_wave, skill_rank, weapon, formation, equips,
+                                          mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
+                                          weapon_stone)
+    silver_damage = sum(silver_attack) // 10
+    urpina_damage = sum(urpina_attack) // 10
+    dual_whirlwind_damage = sum(dual_whirlwind_attack) // 10
+    fancy_lass_damage = sum(fancy_lass_attack) // 10
+    elegant_sands_damage = sum(elegant_sands_attack) // 10
+    imperial_sword_damage = sum(imperial_sword_attack) // 10
+    wheel_swing_damage = sum(wheel_swing_attack) // 10
+    steel_wave_damage = sum(steel_wave_attack) // 10
+    gustave_damage = sum(gustave_attack) // 10
     print(f'Storm Roar:  {silver_damage}')
     print(f'Holy Shining Sword:  {urpina_damage}')
+    print(f'Dual Whirlwind:  {dual_whirlwind_damage}')
     print(f'Lunar Blade+:  {fancy_lass_damage}')
+    print(f'Elegant Sands:  {elegant_sands_damage}')
+    print(f'Imperial Sword:  {imperial_sword_damage}')
+    print(f'Wheel Swing+:  {wheel_swing_damage}')
+    print(f'Steel Wave (Noel):  {steel_wave_damage}')
+    print(f'Steel Wave (Gustave):  {gustave_damage}')
 
 
 def test_shining_holy_sword():
@@ -337,12 +391,43 @@ def test_shining_holy_sword():
     find_skill_power('Urpina', "[I'm Ready]", 'Holy Shining Sword', 6, 41, 221, 0, 0, shining_sword)
 
 
+def georg_test():
+    styles = load_styles()
+    json_character = {'name': 'Georg', 'id': '00003'}
+    georg = Character.Character(json_character)
+    georg.add_styles(styles)
+    georg.update_base_stats(125)
+    ss_style = [style for style in georg.styles if style.style_name == "[Burden of Royalty]"][0]
+    final_strike = [skill for skill in ss_style.skills if skill.name == 'Final Strike'][0]
+    final_strike.power_number = 39
+    equips = Equipment.EquipmentBonus()
+    equips.str = 18
+    weapon = Equipment.Weapon("Khamsin")
+    weapon.type = Common.WeaponType.Greatsword
+    weapon.max_wp = 45
+    formation = Common.FormationBonus()
+    formation.str = 50
+    skill_rank = 99
+    enemy_end = 213
+    enemy_will = 213
+    mastery_rank = 41
+    enemy_resist = -35
+    turn_number = 1
+    full_hp = turn_number == 1
+    weapon_stone = 20
+    final_strike_attack = sum(georg.attack(ss_style, final_strike, skill_rank, weapon, formation, equips,
+                                  mastery_rank, enemy_end, enemy_will, enemy_resist, turn_number, full_hp,
+                                  weapon_stone)) // 10
+    print(f'Final Strike: {final_strike_attack}')
+
+
 if __name__ == '__main__':
     test_fire_feather()
     test_vortex_breaker()
     test_luna_fulgur()
     test_doll_dance()
-    # luna_fulgur_v_cross_break()
+    luna_fulgur_v_cross_break()
     test_cross_break()
     test_shining_holy_sword()
-    # lunar_blade_vs_urps()
+    lunar_blade_vs_urps()
+    georg_test()
